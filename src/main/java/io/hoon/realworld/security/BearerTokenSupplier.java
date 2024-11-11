dcbf4a3695f4335c434eb5dd579d0cf3f5c34c98
hoonsmemory <dltjdgnsok1@naver.com>
2024-11-11 11:36:04 +0900

    feature : 토큰 생성 메서드 추가

---
+++ src/main/java/io/hoon/realworld/security/BearerTokenSupplier.java
@@ -0,0 +1,34 @@
+package io.hoon.realworld.security;
+
+import io.hoon.realworld.domain.user.User;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.oauth2.jwt.JwtClaimsSet;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
+import org.springframework.stereotype.Component;
+
+import java.time.Instant;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class BearerTokenSupplier {
+    private final JwtEncoder jwtEncoder;
+
+    // JWT 토큰을 생성하는 메서드
+    public String supply(User user) {
+        Instant now = Instant.now(); // 현재 시간
+        JwtClaimsSet claimsSet = JwtClaimsSet.builder()
+                                             .issuer("https://realworld.hoon.io") // 발행자
+                                             .issuedAt(now) // 발행 시간
+                                             .expiresAt(now.plusSeconds(300)) // 만료 시간 (300초 후)
+                                             .subject(user.getId().toString()) // 주제 (사용자 ID)
+                                             .build();
+
+        JwtEncoderParameters parameters = JwtEncoderParameters.from(claimsSet);
+        String token = jwtEncoder.encode(parameters).getTokenValue(); // JWT 토큰 인코딩
+        log.info("Generated bearer token with user id `{}`: {}", user.getId(), token); // 토큰 생성 로그
+        return token; // 생성된 토큰 반환
+    }
+}
---
+++ src/test/java/io/hoon/realworld/security/BearerTokenSupplierTest.java
@@ -0,0 +1,45 @@
+package io.hoon.realworld.security;
+
+import io.hoon.realworld.IntegrationTestSupport;
+import io.hoon.realworld.domain.user.User;
+import io.hoon.realworld.domain.user.UserRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class BearerTokenSupplierTest extends IntegrationTestSupport {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    BearerTokenSupplier bearerTokenSupplier;
+
+    @Test
+    @DisplayName("회원 정보를 이용하여 JWT 토큰을 생성한다.")
+    void Supply() {
+        // Given
+        // -- 회원가입
+        String userName = "hoon";
+        String email = "hoon@email.com";
+        String password = "password";
+        User user = User.create(email, userName, password);
+        userRepository.save(user);
+
+        // -- 회원 조회
+        Optional<User> byEmail = userRepository.findByEmail(email);
+
+        // When
+        String token = bearerTokenSupplier.supply(byEmail.get());
+
+        // Then
+        assertNotNull(token);
+        assertTrue(token.startsWith("eyJ"));
+    }
+
+}
