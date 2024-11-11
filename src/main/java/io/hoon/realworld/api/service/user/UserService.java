f413091314ea7ae12babf90b03e6e7e6b2dae0ab
hoonsmemory <dltjdgnsok1@naver.com>
2024-11-11 11:42:43 +0900

    feature : login 서비스 구현

--- src/main/java/io/hoon/realworld/api/service/user/UserService.java
+++ src/main/java/io/hoon/realworld/api/service/user/UserService.java
@@ -1,9 +1,12 @@
 package io.hoon.realworld.api.service.user;
 
+import io.hoon.realworld.api.service.user.request.UserLoginServiceRequest;
 import io.hoon.realworld.api.service.user.request.UserSignUpServiceRequest;
 import io.hoon.realworld.api.service.user.response.UserSingleResponse;
+import io.hoon.realworld.security.BearerTokenSupplier;
 import io.hoon.realworld.domain.user.User;
 import io.hoon.realworld.domain.user.UserRepository;
+import io.hoon.realworld.exception.Error;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -16,13 +19,26 @@ public class UserService {
 
     private final PasswordEncoder passwordEncoder;
     private final UserRepository userRepository;
+    private final BearerTokenSupplier bearerTokenSupplier;
 
     @Transactional
     public UserSingleResponse signUp(UserSignUpServiceRequest request) {
         User user = request.toEntity();
         user.encodePassword(passwordEncoder);
+
         User savedUser = userRepository.save(user);
         return UserSingleResponse.of(savedUser);
+    }
+
+    public UserSingleResponse login(UserLoginServiceRequest request) {
+        return userRepository.findByEmail(request.getEmail())
+                             .filter(user -> passwordEncoder.matches(request.getPassword(), user.getPassword()))
+                             .map(user -> {
+                                 String token = bearerTokenSupplier.supply(user);
+                                 user.possessToken(token);
+                                 return UserSingleResponse.of(user);
+                             })
+                             .orElseThrow(() -> new IllegalArgumentException(Error.INVALID_EMAIL_OR_PASSWORD.getMessage()));
     }
 
 }
--- src/test/java/io/hoon/realworld/api/service/user/UserServiceTest.java
+++ src/test/java/io/hoon/realworld/api/service/user/UserServiceTest.java
@@ -1,10 +1,12 @@
 package io.hoon.realworld.api.service.user;
 
 import io.hoon.realworld.IntegrationTestSupport;
+import io.hoon.realworld.api.service.user.request.UserLoginServiceRequest;
 import io.hoon.realworld.api.service.user.request.UserSignUpServiceRequest;
 import io.hoon.realworld.api.service.user.response.UserSingleResponse;
 import io.hoon.realworld.domain.user.User;
 import io.hoon.realworld.domain.user.UserRepository;
+import io.hoon.realworld.exception.Error;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,13 +23,13 @@ class UserServiceTest extends IntegrationTestSupport {
 
     @Test
     @DisplayName("신규 화원을 등록한다.")
-    void test() throws Exception {
+    void singUp() throws Exception {
         // Given
         String userName = "hoon";
         String email = "hoon@email.com";
         String password = "password";
 
-    	UserSignUpServiceRequest request = UserSignUpServiceRequest.builder()
+        UserSignUpServiceRequest request = UserSignUpServiceRequest.builder()
                                                                    .username(userName)
                                                                    .email(email)
                                                                    .password(password)
@@ -37,12 +39,48 @@ class UserServiceTest extends IntegrationTestSupport {
         UserSingleResponse response = userService.signUp(request);
 
         // Then
-        User byEmail = userRepository.findByEmail(email);
+        User byEmail = userRepository.findByEmail(email)
+                                     .orElseGet(() -> {
+                                         throw new IllegalArgumentException(Error.USER_NOT_FOUND.getMessage());
+                                     });
+
         assertThat(response)
                 .extracting("email", "username", "password")
                 .contains("hoon", "hoon@email.com", byEmail.getPassword());
+    }
 
+    @Test
+    @DisplayName("로그인한다.")
+    void login() throws Exception {
+        // Given
+        String userName = "hoon";
+        String email = "hoon@email.com";
+        String password = "password";
 
+        // - 회원가입
+        UserSignUpServiceRequest signUpRequest = UserSignUpServiceRequest.builder()
+                                                                         .username(userName)
+                                                                         .email(email)
+                                                                         .password(password)
+                                                                         .build();
+
+        userService.signUp(signUpRequest);
+
+        // - 로그인
+        UserLoginServiceRequest loginRequest = UserLoginServiceRequest.builder()
+                                                                      .email(email)
+                                                                      .password(password)
+                                                                      .build();
+
+        // When
+        UserSingleResponse response = userService.login(loginRequest);
+
+        // Then
+        assertThat(response)
+                .extracting("email", "username")
+                .contains(email, userName);
+
+        assertThat(response.getToken()).isNotNull().matches("^[A-Za-z0-9-_=]+\\.[A-Za-z0-9-_=]+\\.[A-Za-z0-9-_.+/=]*$");
     }
 
 }
