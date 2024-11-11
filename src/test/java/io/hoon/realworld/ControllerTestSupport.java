c3989e1149888651752a12d6fc217d29e5b3d06c
hoonsmemory <dltjdgnsok1@naver.com>
2024-11-11 15:02:01 +0900

    refactor : TestConfig 제거

--- src/test/java/io/hoon/realworld/ControllerTestSupport.java
+++ src/test/java/io/hoon/realworld/ControllerTestSupport.java
@@ -1,22 +1,22 @@
 package io.hoon.realworld;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
 import io.hoon.realworld.api.controller.user.UserController;
 import io.hoon.realworld.api.service.user.UserService;
-import io.hoon.realworld.config.SecurityConfig;
+import io.hoon.realworld.config.AppConfig;
+import io.hoon.realworld.domain.user.UserRepository;
+import io.hoon.realworld.security.SecurityConfig;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.web.servlet.MockMvc;
 
 @WebMvcTest(controllers = {
         UserController.class
 })
-@Import({SecurityConfig.class, ControllerTestSupport.TestConfig.class})
+@Import({SecurityConfig.class, AppConfig.class})
 // 컨트롤러 전용 테스트
 public abstract class ControllerTestSupport {
 
@@ -24,18 +24,12 @@ public abstract class ControllerTestSupport {
     protected MockMvc mockMvc;
 
     @Autowired
+    @Qualifier("objectMapper")
     protected ObjectMapper objectMapper;
 
     @MockBean
     protected UserService userService;
 
-    @TestConfiguration
-    static class TestConfig {
-        @Bean
-        public ObjectMapper objectMapper() {
-            ObjectMapper objectMapper = new ObjectMapper();
-            objectMapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
-            return objectMapper;
-        }
-    }
+    @MockBean
+    protected UserRepository userRepository;
 }
